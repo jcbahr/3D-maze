c8459d811ae8083a41a2316078016961aaeff4a1
Jackson Bahr <jake.bahr@gmail.com>
2018-04-20 13:20:09 -0700

    Small change

--- maze.py
+++ maze.py
@@ -4,6 +4,8 @@
 # maze.py
 
 
+# Copyright 2014, Jackson Bahr
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
